19714a542a35fd46634e69d747c302da6fc62b40
andendeun <andendeun@naver.com>
2025-04-15 22:15:51 +0900

    250415_22:00 update

--- log_emotion.py
+++ log_emotion.py
@@ -1,6 +1,6 @@
-from backend.db import save_message
+from backend.db import init_db
 
 def log_emotion(username, emotion, message):
     # 감정 라벨을 포함해서 저장
     formatted_message = f"[{emotion}] {message}"
-    save_message(username, "user", formatted_message)
+    init_db(username, "user", formatted_message)
