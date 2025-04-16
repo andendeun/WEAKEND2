dafc9d05977b8d90bd3344310f2625393319b0a8
andendeun <andendeun@naver.com>
2025-04-16 15:27:07 +0900

    250416_15:00 update

--- backend/auth.py
+++ backend/auth.py
@@ -1,16 +1,16 @@
 from backend.db import supabase
 from datetime import date
 
-def register(user_id, password, birthdate, regionid, phonenumber, gender):
+def register(login_id, password, birthdate, regionid, phonenumber, gender):
     try:
-        result = supabase.table("users").select("username").eq("username", id).execute()
+        result = supabase.table("users").select("username").eq("username", login_id).execute()
         if len(result.data) > 0:
             return False  # 이미 존재
 
          # today = date.today().isoformat()
 
         supabase.table("users").insert({
-            "username": id,
+            "username": login_id,
             "password": password,
             "birthdate": birthdate,
             "region": regionid,
@@ -23,13 +23,13 @@ def register(user_id, password, birthdate, regionid, phonenumber, gender):
         print("❌ 회원가입 실패:", e)
         return False
 
-def login(user_id, password):
-    result = supabase.table("users").select("password").eq("id", user_id).execute()
+def login(login_id, password):
+    result = supabase.table("users").select("password").eq("login_id", login_id).execute()
     if len(result.data) == 0:
         return False
     if result.data[0]["password"] == password:
         # 로그인 성공 시 last_activity 업데이트
         today = str(date.today())
-        supabase.table("users").update({"last_activity": today}).eq("id", id).execute()
+        supabase.table("users").update({"last_activity": today}).eq("login_id", id).execute()
         return True
     return False
