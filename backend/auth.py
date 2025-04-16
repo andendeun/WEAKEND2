9938db2cf1fe26463e67b65b9dd0e1b45ccbc237
andendeun <andendeun@naver.com>
2025-04-16 15:20:43 +0900

    250416_15:00 update

--- backend/auth.py
+++ backend/auth.py
@@ -1,13 +1,5 @@
-import os
-from supabase import create_client
-from dotenv import load_dotenv
+from backend.db import supabase
 from datetime import date
-
-load_dotenv()
-
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
-supabase = create_client(url, key)
 
 def register(user_id, password, birthdate, regionid, phonenumber, gender):
     try:
