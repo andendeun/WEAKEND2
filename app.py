16bfdb5b53731e3ce4ed658e9e5c82874915c415
andendeun <andendeun@naver.com>
2025-04-18 11:27:11 +0900

    250418_12:00 update

--- app.py
+++ app.py
@@ -279,11 +279,12 @@ def show_main_page():
 # ─────────────────────────────────────────────────────────────────────────────
 if not st.session_state["logged_in"]:
     # 로그인 안 된 상태 => 로그인 페이지
-    st.session_state["username"] = "test002"     
+    st.session_state["username"] = "test002"   
+    st.session_state["password"] = "test002"  
     st.session_state["logged_in"] = True        # 테스트용 모드
 
     # show_login_page()        # 실제 배포 시
-    
+
 else:
     # 로그인 성공 상태 => 메인 페이지
     show_main_page()
