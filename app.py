111db8dc73712b6278f05e14bd6727df462bf5b9
andendeun <andendeun@naver.com>
2025-04-18 11:32:20 +0900

    250418_12:00 update

--- app.py
+++ app.py
@@ -277,6 +277,8 @@ def show_main_page():
 # ─────────────────────────────────────────────────────────────────────────────
 # 3) 최종 실행 로직
 # ─────────────────────────────────────────────────────────────────────────────
+
+# TEST용 로그인 모드
 if "logged_in" not in st.session_state:
     st.session_state["logged_in"] = False
 
@@ -285,7 +287,15 @@ if not st.session_state["logged_in"]:
     st.session_state["password"] = "test002"
     st.session_state["logged_in"] = True
 
-# ✅ 임시 확인용
 st.write("로그인된 사용자:", st.session_state.get("username"))
 show_main_page()
 
+
+# # 배포 시
+# if "logged_in" not in st.session_state:
+#     st.session_state["logged_in"] = False
+
+# if not st.session_state["logged_in"]:
+#     show_login_page()
+# else:
+#     show_main_page()
