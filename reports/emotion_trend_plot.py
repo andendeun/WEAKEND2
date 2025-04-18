5c2f12e967ff867fe9209945416f7327e3199943
andendeun <andendeun@naver.com>
2025-04-18 11:14:08 +0900

    250418_12:00 update

--- reports/emotion_trend_plot.py
+++ reports/emotion_trend_plot.py
@@ -1,6 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from .generate_report import get_emotion_report
+import os
+import gdown
+import matplotlib.font_manager as fm
+
+# 폰트 다운로드 및 설정
+font_dir = "./fonts"
+font_path = os.path.join(font_dir, "malgun.ttf")
+os.makedirs(font_dir, exist_ok=True)
+
+if not os.path.exists(font_path):
+    gdown.download(
+        "https://drive.google.com/uc?id=17YAJTJCyK1ZILSY2n1luosc-uHOCTp-b", 
+        font_path,
+        quiet=False
+    )
+
+# matplotlib에 적용
+if os.path.exists(font_path):
+    fontprop = fm.FontProperties(fname=font_path)
+    plt.rcParams["font.family"] = fontprop.get_name()
+    plt.rcParams["axes.unicode_minus"] = False
 
 def plot_emotion_trend(login_id: str, start_date, end_date) -> plt.Figure:
     df = get_emotion_report(login_id)
