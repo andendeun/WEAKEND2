c8fc5dd3dd4ebccc51bc96b72f629b759f5e19db
andendeun <andendeun@naver.com>
2025-04-22 14:21:00 +0900

    250422 update

--- reports/__init__.py
+++ reports/__init__.py
@@ -1,10 +1,10 @@
 # reports/__init__.py
+"""
+Package for report utilities.
+"""
+# 기본 리포트 기능만 노출
+from .generate_report import get_emotion_report
+from .generate_report import create_pdf_report
 
-from .emotion_trend_plot    import plot_emotion_trend
-from .generate_report       import get_emotion_report, create_pdf_report
-
-__all__ = [
-    "plot_emotion_trend",
-    "get_emotion_report",
-    "create_pdf_report",
-]
+# emotion_trend_plot 모듈은 필요 시 명시적으로 import
+#__all__ = ['get_emotion_report', 'create_pdf_report']
